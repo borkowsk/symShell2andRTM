731947fb164ed9ef03455b62544f349085cd5177
Wojciech T. Borkowski <wborkowsk@gmail.com>
2026-03-28 17:21:42 +0100

    Przykodowanie na UTF-8 c.d.

--- EXAMPLES/life/src/lworldimpi.cpp
+++ EXAMPLES/life/src/lworldimpi.cpp
@@ -1,6 +1,6 @@
 /// @file
 /// Implementation of the lifeworld class - virtual Input method
-//---------------------------------------------------------------
+//--------------------------------------------------------------
 /// Its is rather simply implementation. You can replace it if you need.
 // /////////////////////////////////////////////////////////////////////
 /// @date 2026-03-27 (modified)
