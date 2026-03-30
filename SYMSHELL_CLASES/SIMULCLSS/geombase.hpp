77a2a190a92770c78273e491fa1e0a3a432f56dd
Wojciech T. Borkowski <wborkowsk@gmail.com>
2026-03-30 18:47:37 +0200

    Konwersja na UTF-8 i usunięcie tabulacji we wcięciach

--- SYMSHELL_CLASES/SIMULCLSS/geombase.hpp
+++ SYMSHELL_CLASES/SIMULCLSS/geombase.hpp
@@ -1,14 +1,14 @@
-////////////////////////////////////////////////////////////////////////////////
-//GEOMETRY - SPOSOB ORGANIZACJI AGENTOW W WARSTWIE
-////////////////////////////////////////////////////////////////////////////////
-//Geometria jest obiektem kt�ry potrafii opisac po�ozenie agent�w w warstwie i
-// wzajemnie wzgledem siebie i przetworzyc je na liniowy indeks tablicy agent�w
-// INTERFACE dla geometrii swiata symulacji
-////////////////////////////////////////////////////////////////////////////////
+/// @file
+/// GEOMETRIA - SPOSÓB ORGANIZACJI AGENTÓW W WARSTWIE.
+// ////////////////////////////////////////////////////
+/// @date 2026-03-30 (modified)
+/// Geometria jest obiektem, który potrafi opisać położenie agentów w warstwie,
+/// a także wzajemnie względem siebie i przetworzyć je na liniowy indeks tablicy.
+/// /////////////////////////////////////////////////////////////////////////////
 #ifndef __GEOMBASE_HPP__
 #define __GEOMBASE_HPP__
 
-#include <stddef.h>
+#include <cstddef.h>
 #include <assert.h>
 #include <limits.h>
 #include <float.h>
@@ -16,10 +16,6 @@
 
 //#include "platform.hpp"
 #include <iostream>
-
-//#ifdef F U L L
-//#define F U L L ((unsigned long)(-1)) //Moze byc w konflikcie z innymi definicjami FULL - np ((void*)0xffffffff)
-//#endif
 
 //Typ uchwytowy do iteratorow geometri
 //---------------------------------------
@@ -39,82 +35,83 @@ operator void* () {return (void*)val;}
 };
 */
 
+/// INTERFACE dla geometrii świata symulacji.
 class geometry_base 
 //-----------------
 {
 public:
     enum my_full {FULL=((unsigned long)(-1))}; //Zamiast #define FULL
 
-	//LOKALNE KLASY i STRUKTURY
-	//-------------------------------------
+    //LOKALNE KLASY i STRUKTURY
+    //-------------------------------------
 
-	//Struktura dla wyrazania wspo�rzednych
-	union coord
-	//---------------
-	{
-		//struct{double X,Y,Z,T,U,V;};
-		double C[6];
-		coord()
-		{	C[0]=C[1]=C[2]=C[3]=C[4]=C[5]=0;	} //initial values
-		double& X(){ return C[0];}
-		double& Y(){ return C[1];}
-		double& Z(){ return C[2];}
-		double& T(){ return C[3];}
-		double& U(){ return C[4];}
-		double& V(){ return C[5];}
-		/* Czasem kompilator ma problem ktora wybrac - const czy nie const */
-		double X()const { return C[0];}
-		double Y()const { return C[1];}
-		double Z()const { return C[2];}
-		double T()const { return C[3];}
-		double U()const { return C[4];}
-		double V()const { return C[5];}
-	};
-		
-	struct MD_info
-	//-------------
-	{	
-		coord min;	//Najmniejsze wartosci dla kazdej wspolrzednej
-		coord max;	//Najwieksze wartosci dla kazdej wspolrzednej	
-	};
-	
-	struct view_info
-	//---------------
-	{
-		coord pos;//Pozycja oka kamery
-		coord dia;//Na jaka odleglosc chce widziec kamera	
-		coord sst;//Sugerowany minimalny krok przesuwu
-	};
-	
-	//Klasa bazowa dla wszystkich iteratorow geometrii
-	class iterator_base
-	//------------------
-	{
-	protected:
-		size_t items;	//Ile elementow - trzeba ustawic!!!
-		unsigned marker;//Dla sprawdzania ze to faktycznie iterator
+    //Struktura dla wyrazania wspo�rzednych
+    union coord
+    //---------------
+    {
+        //struct{double X,Y,Z,T,U,V;};
+        double C[6];
+        coord()
+        {	C[0]=C[1]=C[2]=C[3]=C[4]=C[5]=0;	} //initial values
+        double& X(){ return C[0];}
+        double& Y(){ return C[1];}
+        double& Z(){ return C[2];}
+        double& T(){ return C[3];}
+        double& U(){ return C[4];}
+        double& V(){ return C[5];}
+        /* Czasem kompilator ma problem ktora wybrac - const czy nie const */
+        double X()const { return C[0];}
+        double Y()const { return C[1];}
+        double Z()const { return C[2];}
+        double T()const { return C[3];}
+        double U()const { return C[4];}
+        double V()const { return C[5];}
+    };
 
-	public:
-		//Optymalizacja alokacji!!!
-		  void* operator new (size_t s);
-		  void  operator delete (void* p, size_t s);
+    struct MD_info
+    //-------------
+    {
+        coord min;	//Najmniejsze wartosci dla kazdej wspolrzednej
+        coord max;	//Najwieksze wartosci dla kazdej wspolrzednej
+    };
 
-		//Konstruktor ustawia marker
-		iterator_base(size_t iite):
-				marker(0xfedcba00),items(iite){}
+    struct view_info
+    //---------------
+    {
+        coord pos;//Pozycja oka kamery
+        coord dia;//Na jaka odleglosc chce widziec kamera
+        coord sst;//Sugerowany minimalny krok przesuwu
+    };
 
-	virtual	//Destruktor - na wszelki wypadek wirtualny
-		~iterator_base()	{ marker=0;}
+    //Klasa bazowa dla wszystkich iteratorow geometrii
+    class iterator_base
+    //------------------
+    {
+    protected:
+        size_t items;	//Ile elementow - trzeba ustawic!!!
+        unsigned marker;//Dla sprawdzania ze to faktycznie iterator
 
-		  //Czy to na pewno iterator - np dla assercji
-		  int is_iterator()	{ return marker==0xfedcba00;} 
-		  
-		  //Ile jest elementow w iteracji
-		  size_t number_of_items(){ return items;}
-		  
-		  //Implementacja pobrania nastepnego elementu
-		  virtual void _next(const geometry_base&,size_t& ret,size_t& end)=0;		  
-	};
+    public:
+        //Optymalizacja alokacji!!!
+          void* operator new (size_t s);
+          void  operator delete (void* p, size_t s);
+
+        //Konstruktor ustawia marker
+        iterator_base(size_t iite):
+                marker(0xfedcba00),items(iite){}
+
+    virtual	//Destruktor - na wszelki wypadek wirtualny
+        ~iterator_base()	{ marker=0;}
+
+          //Czy to na pewno iterator - np dla assercji
+          int is_iterator()	{ return marker==0xfedcba00;}
+
+          //Ile jest elementow w iteracji
+          size_t number_of_items(){ return items;}
+
+          //Implementacja pobrania nastepnego elementu
+          virtual void _next(const geometry_base&,size_t& ret,size_t& end)=0;
+    };
 
 private:
 int dimension;	//Ilosc wymiar�w geometri - zeby uzyc typu "coord"
@@ -123,7 +120,7 @@ protected:
 
 //Sprawdza czy VMT i dimension sa takie same
 int _compare_geometry_base(geometry_base* sec);
-	
+
 public:			  
 //METODY INFORMACYJNE O
 /////////////////////////////////////////////////////////////
@@ -151,17 +148,17 @@ int			set_view_info(const view_info*)=0;
 virtual //Mozna przedefiniowywac jesli sa powody!!!
 int  compare(geometry_base& sec)
 {
-	return _compare_geometry_base(&sec);
+    return _compare_geometry_base(&sec);
 }
 
 int operator == (geometry_base& sec)
 {
-	return compare(sec)==0;
+    return compare(sec)==0;
 }
 
 int operator != (geometry_base& sec)
 {
-	return compare(sec)!=0;
+    return compare(sec)!=0;
 }
 
 //METODY TWORZACE ITERATORY DO PRZECHODZENIA GEOMETRII - PURE VIRTUAL!!!
@@ -200,11 +197,11 @@ void destroy_iterator(iteratorh& p) const;
 
 //CONSTRUCTION/DESTRUCTION
 //--------------------------------
-	geometry_base(int ini):
-		dimension(ini){}
+    geometry_base(int ini):
+        dimension(ini){}
 
 virtual
-	~geometry_base(){}
+    ~geometry_base(){}
 
 };
 
@@ -218,7 +215,7 @@ typedef geometry_base geometry;
 inline
 int geometry_base::_compare_geometry_base(geometry_base* sec)
 {	//Sprawdza czy VMT i dimension sa takie same
-	return memcmp(this,sec,sizeof(geometry_base));
+    return memcmp(this,sec,sizeof(geometry_base));
 }
 
 inline
@@ -226,22 +223,22 @@ size_t geometry_base::get_next(iteratorh& p) const
 //Zwraca indeks do aktualnego i przesuwa iterator.
 //Zeruje iterator jesli koniec danych.Zwraca uFULL jesli missing...
 {
-	size_t ret=FULL,end=0;
+    size_t ret=FULL,end=0;
     
-	iterator_base* pom=(iterator_base*)p;//NIELADNY CHWYT ALE DZIALA
+    iterator_base* pom=(iterator_base*)p;//NIELADNY CHWYT ALE DZIALA
     //iterator_base* pom1=(iterator_base*)(&p); assert(pom1==pom); --> A TAK NIE DZIALA
                                     assert(pom!=NULL);                                    
-	                                assert(pom->is_iterator());
-	
-	//Wlasciwe odczytanie i przesuniecie iteratora
-	pom->_next(*this,ret,end);
-	if(end)
-	{
-		assert(pom->is_iterator());
-		destroy_iterator(p);//Usuwany iterator 
-	}
-	
-	return ret;
+                                    assert(pom->is_iterator());
+
+    //Wlasciwe odczytanie i przesuniecie iteratora
+    pom->_next(*this,ret,end);
+    if(end)
+    {
+        assert(pom->is_iterator());
+        destroy_iterator(p);//Usuwany iterator
+    }
+
+    return ret;
 }
 
 
@@ -249,12 +246,12 @@ inline
 void    geometry_base::destroy_iterator(iteratorh& p) const
 //Likwiduje iterator, ktoremu nie dano dojsc do konca
 {
-	if(p==NULL) return;
-	void* ptr=p;//Uwolnic sama warto��
-	iterator_base* pom=(iterator_base*)ptr;//NIELADNY CHWYT ALE DZIALA
-	assert(pom->is_iterator());            //choc dobrze sprawdzic czy faktycznie
-	delete pom;//Usuwany iterator
-	p=NULL;
+    if(p==NULL) return;
+    void* ptr=p;//Uwolnic sama warto��
+    iterator_base* pom=(iterator_base*)ptr;//NIELADNY CHWYT ALE DZIALA
+    assert(pom->is_iterator());            //choc dobrze sprawdzic czy faktycznie
+    delete pom;//Usuwany iterator
+    p=NULL;
 }
 
 inline //Informacja o maksymalnej mozliwej odleglosci - potrzebna np dla "Spatial correlation"
