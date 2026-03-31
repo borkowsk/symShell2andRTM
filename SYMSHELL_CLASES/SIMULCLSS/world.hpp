e33ba99f2e40b5c4f5062e44497f73efab5865ab
Wojciech T. Borkowski <wborkowski@uw.edu.pl>
2026-03-31 14:54:28 +0200

    Spacje w komentarzu końcowym

--- SYMSHELL_CLASES/SIMULCLSS/world.hpp
+++ SYMSHELL_CLASES/SIMULCLSS/world.hpp
@@ -204,15 +204,19 @@ area_menager&		world::MyAreaMenager()
 }
 
 #endif // !defined(WORLD_HPP_INCLUDED_)
-/* **************************************************************** */
+
+/********************************************************************/
 /*           THIS CODE IS DESIGNED & COPYRIGHT  BY:                 */
 /*            W O J C I E C H   B O R K O W S K I                   */
-/* Zakład Systematyki i Geografii Roslin Uniwersytetu Warszawskiego */
-/*  & Instytut Studiów Społecznych Uniwersytetu Warszawskiego       */
-/*        WWW:  http://moderato.iss.uw.edu.pl/~borkowsk             */
-/*        MAIL: borkowsk@iss.uw.edu.pl                              */
+/*                                                                  */
+/*      Instytut Studiów Społecznych Uniwersytetu Warszawskiego     */
+/*                                                                  */
+/*        WWW:  http://www.iss.uw.edu.pl/~borkowsk                  */
+/*        MAIL: borkowsk@samba.iss.uw.edu.pl                        */
+/*                                                                  */
 /*                               (Don't change or remove this note) */
-/* **************************************************************** */
+/********************************************************************/
+
 
 
 
