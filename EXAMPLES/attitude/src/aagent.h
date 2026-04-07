9faf4807a592655b710f03a94cd2afc9efe43766
Wojciech T. Borkowski <wborkowsk@gmail.com>
2026-04-07 19:03:38 +0200

    Blank characters magic

--- EXAMPLES/attitude/src/aagent.h
+++ EXAMPLES/attitude/src/aagent.h
@@ -1,107 +1,117 @@
 // DECLARATION OF    A G E N T   FOR "attitudeS" SIMULATION
-/////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////
 #include "layer.hpp"
 
 class aagent:public agent_base
 {
-	friend class aworld;//Na razie tak - zeby uproscic dostep do skladowych.
-	
-	// STATYCZNE SKLADOWE - PARAMETRY INICJOWANIA AGENTÓW
-	static short ruchsily;//Czy sila sie zmienia (rosnie) z wiekiem
-	static short max_sila;//Maksymalna sila agenta
-	static short ile_kate;//Ilosc kategori w mapach	
-	static short kate_shift;//Przesuniecie dla wczytywania gifa
-	static double MutationLevel;//Prawd. spontanicznej zmiany pogladow (0..1)
-	
-	// SKLADOWE DLA SYMULACJI
-	short Power;	//Sila agenta
-	short First;	//Pierwsze przekonanie
-	short Second;	//Nowe przekonanie
-	
-	
-	void _clean()
-	{
-		First=-1;
-		Second=-1;
-		Power=-1;
-	}
-	
-	// TO CO MUSI byc zdefiniowane
-	///////////////////////////////////
+    friend class aworld; //Na razie tak - zeby uproscic dostep do skladowych.
+
+    // STATYCZNE SKLADOWE - PARAMETRY INICJOWANIA AGENTÓW
+    static short ruchsily; //Czy sila sie zmienia (rosnie) z wiekiem
+    static short max_sila; //Maksymalna sila agenta
+    static short ile_kate; //Ilosc kategori w mapach
+    static short kate_shift; //Przesuniecie dla wczytywania gifa
+    static double MutationLevel; //Prawd. spontanicznej zmiany pogladow (0..1)
+
+    // SKLADOWE DLA SYMULACJI
+    short Power;	//Sila agenta
+    short First;	//Pierwsze przekonanie
+    short Second;	//Nowe przekonanie
+
+
+    void _clean()
+    {
+        First=-1;
+        Second=-1;
+        Power=-1;
+    }
+
+    // TO CO MUSI byc zdefiniowane
+    // /////////////////////////////////
 public:
-	int IsOK()
-	{
-		return First!=-1 && Second!=-1 && Power!=-1;
-	}
-	
-	void MakeOlder()//Sila jako wiek
-	{
-		if(aagent::ruchsily)
-		{
-			Power+=aagent::ruchsily;
-			Power%=aagent::max_sila;//Nigdy nie przekracza sily maksymalnej
-		}
-	}
+    int IsOK()
+    {
+        return First!=-1 && Second!=-1 && Power!=-1;
+    }
 
-	aagent(const aagent& ini);	//Konkretna implementacja w aworld!
-	
-	aagent();					//Konkretna implementacja w aworld!
+    void MakeOlder()//Sila jako wiek
+    {
+        if(aagent::ruchsily)
+        {
+            Power+=aagent::ruchsily;
+            Power%=aagent::max_sila; //Nigdy nie przekracza sily maksymalnej
+        }
+    }
 
-	aagent* clone() const
-	{ return new aagent(*this);}
-		
-	~aagent()
-	{_clean();}
-	
-	void clean()
-	{_clean();}
-	
-	void assign123(unsigned char Red,unsigned char Green,unsigned char Blue)
-	{
-		First=Red>>kate_shift;
-		Second=Blue>>kate_shift;	
-	}
-	
-	void assignPow(unsigned char Red,unsigned char Green,unsigned char Blue)
-	{
-		Power=short((int(Red)+int(Green)+int(Blue))/(3.*255)*max_sila);
-	}
-	
-	void killBlack(unsigned char Red,unsigned char Green,unsigned char Blue)
-	{
-		if(Red==0 && Green==0 && Blue==0)
-			_clean();
-	}
-	
-	long Classif()
-	{
-		return First;
-	}
-	
-	long RGB()
-	{
-		return (unsigned long) ( (unsigned char) (First) );
-	}
+    aagent(const aagent& ini);	//Konkretna implementacja w aworld!
 
-	friend
-	ostream& operator << (ostream& o,const aagent& a)
-	{
-		o<<'{';
-		o<<' '<<a.Power<<' '<<a.First<<' '<<a.Second<<' ';	
-		o<<'}';
-		return o;
-	}
-	
-	friend
-	istream& operator >> (istream& i,aagent& a)
-	{
-		char pom;
-		i>>pom;		//ignoruje {
-		i>>a.Power>>a.First>>a.Second;
-		i>>pom;		//ignoruje }
-		return i;
-	}
+    aagent();					//Konkretna implementacja w aworld!
+
+    aagent* clone() const
+    { return new aagent(*this);}
+
+    ~aagent()
+    {_clean();}
+
+    void clean()
+    {_clean();}
+
+    void assign123(unsigned char Red,unsigned char Green,unsigned char Blue)
+    {
+        First=Red>>kate_shift;
+        Second=Blue>>kate_shift;
+    }
+
+    void assignPow(unsigned char Red,unsigned char Green,unsigned char Blue)
+    {
+        Power=short((int(Red)+int(Green)+int(Blue))/(3.*255)*max_sila);
+    }
+
+    void killBlack(unsigned char Red,unsigned char Green,unsigned char Blue)
+    {
+        if(Red==0 && Green==0 && Blue==0)
+            _clean();
+    }
+
+    long Classif()
+    {
+        return First;
+    }
+
+    long RGB()
+    {
+        return (unsigned long) ( (unsigned char) (First) );
+    }
+
+    friend
+    ostream& operator << (ostream& o,const aagent& a)
+    {
+        o<<'{';
+        o<<' '<<a.Power<<' '<<a.First<<' '<<a.Second<<' ';
+        o<<'}';
+        return o;
+    }
+
+    friend
+    istream& operator >> (istream& i,aagent& a)
+    {
+        char pom;
+        i>>pom;		//ignoruje {
+        i>>a.Power>>a.First>>a.Second;
+        i>>pom;		//ignoruje }
+        return i;
+    }
 
 };
+
+/* **************************************************************** */
+/*           THIS CODE IS DESIGNED & COPYRIGHT BY:                  */
+/*            W O J C I E C H   B O R K O W S K I                   */
+/* Zakład Systematyki i Geografii Roślin Uniwersytetu Warszawskiego */
+/*  & Instytut Studiów Społecznych Uniwersytetu Warszawskiego       */
+/*        WWW:  http://moderato.iss.uw.edu.pl/~borkowsk             */
+/*        MAIL: borkowsk@iss.uw.edu.pl                              */
+/*                               (Don't change or remove this note) */
+/* **************************************************************** */
 
 
--- EXAMPLES/attitude/src/aworld.h
+++ EXAMPLES/attitude/src/aworld.h
@@ -1,5 +1,5 @@
-// DECLARATION OF   W O R L D  FOR "attitudeS" SIMULATION
-/////////////////////////////////////////////////////////
+// DECLARATION OF   W O R L D  FOR "attitudes" SIMULATION
+// ///////////////////////////////////////////////////////
 #include <climits> //SHRT_MAX
 #include "world.hpp"
 #include "layer.hpp"
@@ -8,75 +8,85 @@
 class aworld:public world	//Caly swiat symulacji
 //--------------------------------------------------
 {
-//Parametry jednowartosciowe
-/////////////////////////////////
-size_t				MyWidth;	//Obwod torusa
-short				MaxSila;	//Maksymalna sila agenta
-short				TrsSila;	//Treshold sily powyzej ktorego nie ma zmian
-short				IleKate;	//Ilosc kategori w mapach
-short				IleSasiad;	//8==Gestosc sasiedztwa
-short				OdlSasiad;	//Rozmiar sasiedztwa
-short				BierzWszystko;//Czy ma brac wszystko z sasiedztwa
-short				UseSelf;	//Czy ma brac siebie pod uwage
-double				Noise;		//Szum informacyjny
-bool				Synchronic; //Synchroniczna zmiana pogladow
-wb_pchar			MappName;	//nazwa pliku inicjujacej bitmapy
-wb_pchar			MaplName;	//nazwa pliku inicjujacej bitmapy
-wb_pchar			MaskName;	//nazwa pliku inicjujacej bitmapy
+    //Parametry jednowartosciowe
+    // ///////////////////////////////
+    size_t				MyWidth;	//Obwod torusa
+    short				MaxSila;	//Maksymalna sila agenta
+    short				TrsSila;	//Treshold sily powyzej ktorego nie ma zmian
+    short				IleKate;	//Ilosc kategori w mapach
+    short				IleSasiad;	//8==Gestosc sasiedztwa
+    short				OdlSasiad;	//Rozmiar sasiedztwa
+    short				BierzWszystko; //Czy ma brac wszystko z sasiedztwa
+    short				UseSelf;	//Czy ma brac siebie pod uwage
+    double				Noise;		//Szum informacyjny
+    bool				Synchronic; //Synchroniczna zmiana pogladow
+    wb_pchar			MappName;	//nazwa pliku inicjujacej bitmapy
+    wb_pchar			MaplName;	//nazwa pliku inicjujacej bitmapy
+    wb_pchar			MaskName;	//nazwa pliku inicjujacej bitmapy
 
-//Warstwy symulacji (sa torusami)
-/////////////////////////////////
-//rectangle_unilayer<unsigned char> zdatnosc;//Warstwa definiujaca zdatnosc do zasiedlenia
-rectangle_layer_of_ptr_to_agents<aagent> Agenci;  //Wlaściwa warstwa agentow zasiedlajacych
+    //Warstwy symulacji (sa torusami)
+    // ///////////////////////////////
+    //rectangle_unilayer<unsigned char> zdatnosc; //Warstwa definiujaca zdatnosc do zasiedlenia
+    rectangle_layer_of_ptr_to_agents<aagent> Agenci;  //Wlaściwa warstwa agentow zasiedlajacych
 
-//Glowne serie - wygodniej miec wskazniki niz odszukiwac z Sources po nazwach
-////////////////////////////////////////////////////////////////////////////////
-ptr_to_struct_matrix_source<aagent,short>		*Firsts;//=Agenci.make_source("First mem",&aagent::First);		
-ptr_to_struct_matrix_source<aagent,short>		*Seconds;//=Agenci.make_source("Second mem",&aagent::Second);
+    //Glowne serie - wygodniej miec wskazniki niz odszukiwac z Sources po nazwach
+    // //////////////////////////////////////////////////////////////////////////////
+    ptr_to_struct_matrix_source<aagent,short>		*Firsts; //=Agenci.make_source("First mem",&aagent::First);
+    ptr_to_struct_matrix_source<aagent,short>		*Seconds; //=Agenci.make_source("Second mem",&aagent::Second);
 
-ptr_to_struct_matrix_source<aagent,short>		*Powers;//=Agenci.make_source("Power",&aagent::Power);
-//method_by_ptr_matrix_source<aagent,long>		*Classif;//=Agenci.make_source("Classification",&aagent::Classif);
+    ptr_to_struct_matrix_source<aagent,short>		*Powers; //=Agenci.make_source("Power",&aagent::Power);
+    //method_by_ptr_matrix_source<aagent,long>		*Classif; //=Agenci.make_source("Classification",&aagent::Classif);
 
-int CheckChange(const geometry_base* MyGeom,size_t index,aagent& CenterAgent);
+    int CheckChange(const geometry_base* MyGeom,size_t index,aagent& CenterAgent);
 
 public:
-//KONSTRUKCJA DESTRUKCJA
-aworld(size_t Width,	//Szerokosc torusa macierzy agentow
-	  char* log_name,	//Nazwa pliku do zapisywania histori
-	  char* mapl_name,	//Nazwa (bit)mapy inicjujacej "skladowe"
-	  char* mapp_name,	//Nazwa (bit)mapy inicjujacej "sily"
-	  char* live_mask,	//Czarne w tej mapie sa kasowane
-	  double Noise=0,
-	  short	max_sila=255,//Maksymalna sila agenta
-	  short	ile_kate=256,//Ilosc kategori w mapach	
-	  short	odl_sasiad=1,//Rozmiar sasiedztwa
-	  short	ile_sasiad=8, //8==Gestosc sasiedztwa	- jesli -1 to wszystko po kolei
-	  short need_use_self=0,
-	  bool	synchronicly=true,
-	  short walkpower=0,	//Czy sila rosnie z wiekiem agenta 
-	  short trespower=SHRT_MAX,  //Sila powyzej ktorej agent jest odporny na wplyw
-	  double spontanic=0	//Prawdopodobienstwo spontanicznej zmiany pogladu
-	  );	
+    //KONSTRUKCJA DESTRUKCJA
+    aworld(size_t Width,	//Szerokosc torusa macierzy agentow
+          char* log_name,	//Nazwa pliku do zapisywania histori
+          char* mapl_name,	//Nazwa (bit)mapy inicjujacej "skladowe"
+          char* mapp_name,	//Nazwa (bit)mapy inicjujacej "sily"
+          char* live_mask,	//Czarne w tej mapie sa kasowane
+          double Noise=0,
+          short	max_sila=255, //Maksymalna sila agenta
+          short	ile_kate=256, //Ilosc kategori w mapach
+          short	odl_sasiad=1, //Rozmiar sasiedztwa
+          short	ile_sasiad=8, //8==Gestosc sasiedztwa	- jesli -1 to wszystko po kolei
+          short need_use_self=0,
+          bool	synchronicly=true,
+          short walkpower=0,	//Czy sila rosnie z wiekiem agenta
+          short trespower=SHRT_MAX,  //Sila powyzej ktorej agent jest odporny na wplyw
+          double spontanic=0	//Prawdopodobienstwo spontanicznej zmiany pogladu
+          );
 
-~aworld(){}
+    ~aworld(){}
 
 protected:
-//AKCJE
-void	initialize_layers();	//Stan startowy symulacji
-void	after_read_from_image();//actions after read state from file. Aktualizacja pol static aagent'a!!!
-void	simulate_one_step();	//Wlasciwa implementacja kroku symulacji
+    //AKCJE
+    void	initialize_layers();	//Stan startowy symulacji
+    void	after_read_from_image(); //actions after read state from file. Aktualizacja pol static aagent'a!!!
+    void	simulate_one_step();	//Wlasciwa implementacja kroku symulacji
 
-//Wspolpraca z menagerem wyswietlania
-//---------------------------------------------
-void	make_default_visualisation();//Tworzy domyslne "lufciki" i umieszcza w
-//void actualize_out_area(); // aktualizacja zawartosci OutArea po n krokach symulacji
+    //Wspolpraca z menagerem wyswietlania
+    //---------------------------------------------
+    void	make_default_visualisation(); //Tworzy domyslne "lufciki" i umieszcza w
+    //void actualize_out_area(); // aktualizacja zawartosci OutArea po n krokach symulacji
 
-//Generuje podstawowe zrodla dla wbudowanego menagera danych lub innego
-void	make_basic_sources();
+    //Generuje podstawowe zrodla dla wbudowanego menagera danych lub innego
+    void	make_basic_sources();
 
-//Implementacja wejscia/wyjscia. Zwracaj 1 jesli sukces!
-virtual
-int		implement_output(ostream& o) const;
-virtual
-int		implement_input(istream& i);
+    //Implementacja wejscia/wyjscia. Zwracaj 1 jesli sukces!
+    virtual
+    int		implement_output(ostream& o) const;
+    virtual
+    int		implement_input(istream& i);
 };
+
+/* **************************************************************** */
+/*           THIS CODE IS DESIGNED & COPYRIGHT BY:                  */
+/*            W O J C I E C H   B O R K O W S K I                   */
+/* Zakład Systematyki i Geografii Roślin Uniwersytetu Warszawskiego */
+/*  & Instytut Studiów Społecznych Uniwersytetu Warszawskiego       */
+/*        WWW:  http://moderato.iss.uw.edu.pl/~borkowsk             */
+/*        MAIL: borkowsk@iss.uw.edu.pl                              */
+/*                               (Don't change or remove this note) */
+/* **************************************************************** */
